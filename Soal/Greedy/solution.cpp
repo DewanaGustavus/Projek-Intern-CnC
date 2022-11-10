d8055a0f9c8200263d9ae52a68efd34679753230
louis <louiscaesapdf@gmail.com>
2022-11-10 22:07:06 +0700

    revisi greedy

--- Soal/Greedy/solution.cpp
+++ Soal/Greedy/solution.cpp
@@ -1,8 +1,14 @@
-const int mod = 10000003;
+#include <bits/stdc++.h>
+using namespace std;
 
-int Solution::seats(string A) {
-    int i, sum, currentx, currentxgroup, stepsum;
-    int length = A.size(); // panjang string
+int main() {
+    std::string inp;
+    int i, sum, currentx, currentxgroup;
+    long long stepsum;
+    int mod = 10000003;
+    cin >> inp;
+
+    int length = inp.size(); // panjang string
     
     // KAMUS
     sum = 0; // jumlah orang
@@ -11,14 +17,14 @@ int Solution::seats(string A) {
     
     // ALGORITMA
     for (i = 0; i < length; i++) {
-        if (A[i] == 'x') {
+        if (inp[i] == 'x') {
             sum++;
         }
     }
     
     // secara intuisi kita menggeser X mendekati yang paling banyak untuk meminimasi langkah yang harus diambil
     for (i = 0; i < length; i++) {
-        if (A[i] == 'x') { // jika pada index i adalah x maka jumlahx yang bersebelahan + 1
+        if (inp[i] == 'x') { // jika pada index i adalah x maka jumlahx yang bersebelahan + 1
             currentx++;
         } else {
             // menentukan x mana yang harus digrupkan, tentukan grup yang lebih kecil untuk meminimasi langkah yang diambil
@@ -29,9 +35,11 @@ int Solution::seats(string A) {
             }
             
             // setiap x pada grup di geser sebanyak 1 langkah
-            stepsum = (stepsum + currentxgroup) % mod;
+            stepsum = stepsum + currentxgroup;
         }
     }
     
-    return stepsum;  
+    cout << stepsum;
+
+    return 0;  
 }
