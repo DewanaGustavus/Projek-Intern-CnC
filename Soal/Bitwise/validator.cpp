1f596ae37385dbbf88413559a8f5f912889e7016
michaelu670 <michaelu670@gmail.com>
2023-01-08 16:27:01 +0700

    add validator (temp) to Bitwise

---
+++ Soal/Bitwise/validator.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const double EPS = 1e-5;
+int ac()
+{
+    exit(42);
+}
+
+int wa()
+{
+    exit(43);
+}
+
+int main(int argc, char *argv[])
+{
+    ifstream tc_in(argv[1]);
+    ifstream tc_out(argv[2]);
+
+    int tc;
+    string s_tc;
+    getline(tc_in, s_tc);
+    tc = atoi(s_tc.c_str());
+
+    for (int i = 0; i < tc; i++)
+    {
+        string s_in, s_out;
+        getline(tc_in, s_in);
+        getline(cin, s_out);
+        stringstream str_in(s_in);
+        stringstream str_out(s_out);
+
+        int N, X;
+        string op;
+        str_in >> X >> N >> op;
+
+        int total = 0;
+        if (!(str_out >> total))
+            return wa();
+
+        int temp;
+        for (int i = 1; i < N; i++)
+        {
+            if (!(str_out >> temp))
+                return wa();
+            if (temp < 1 || temp > 1e9)
+                return wa();
+
+            if (op == "AND")
+                total &= temp;
+            else if (op == "OR")
+                total |= temp;
+            else if (op == "XOR")
+                total ^= temp;
+        }
+        if (str_out >> temp)
+            return wa();
+        if (total != X)
+            return wa();
+    }
+    return ac();
+}
